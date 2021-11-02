4fb49f05c598aa0923276c0cf20567119b66c9c7
levanchuong <trumle2k1@gmail.com>
2021-11-02 11:14:10 +0700

    commit server number 4

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 
 let ListRoom = [
-  { nameRoom: 'Phòng số 1', idRoom: '7fd8hfd', coin: 40000, Players: [], playing: false, isTurn: 0 }
+  { nameRoom: 'Phòng số 1', idRoom: '7fd8hfd', coin: 40000, Players: [], playing: false, isTurn: 0, password: 'chuong03022001' }
 ]
 io.on("connection", function (socket) {
   console.log('Kết nối thành công vs ' + socket.id)
@@ -27,140 +27,174 @@ io.on("connection", function (socket) {
   socket.on('joinRoom', (data) => {
     ListRoom.forEach(element => {
       if (element.idRoom === data.idRoom) {
-        if (element.Players.length === 4)
+        if (element.Players.length === element.amount)
           socket.emit('joinError')
         else {
           let joinViewer = false;
           if (element.playing) joinViewer = true;
-          const user = { socketId: socket.id, userName: data.userName, ready: false, Viewer: joinViewer, skip: false, cards: null, totalCards: null}
+          const user = { socketId: socket.id, userName: data.userName, ready: false, Viewer: joinViewer, skip: false, cards: null, totalCards: null, dangdanh: null }
           element.Players.push(user)
           socket.local.emit('joinSuccess', ListRoom)
-          socket.broadcast.emit('updateRoomWhenJoin', user) // cập nhật bàn khi có người mới vào bàn
-          return;
+          element.Players.forEach(item => {
+            io.to(item.socketId).emit('updateRoomWhenJoin', user)
+          })
+          //thiếu return
         }
+        // if (element.Players.length >= 2 && !element.playing) {
+        //   console.log('hello')
+        //   element.Players.forEach(item => {
+        //     io.to(item.socketId).emit('countdown')
+        //   })
+        // }
       }
-    })  
-    if(ListRoom[0].Players.length >= 2 && ListRoom[0].playing === false) {
-      io.sockets.emit('countdown');
-    }
+    })
   })
   socket.on('createRoom', (data) => {
-    ListRoom.push({ nameRoom: data.nameRoom, idRoom: 'kfdfdh39', numberOfUsers: 0, coin: 40000, Players: [] })
-  })
+    const room = { nameRoom: data.nameroom, idRoom: 'fdfdfd', coin: data.coin, Players: [], playing: false, amount: data.amount, isTurn: 0, password: data.password }
+    ListRoom.push(room)
+    io.sockets.emit('updateRoom', getRoom(ListRoom))
+  })  
   socket.on('startGame', (data) => {
-    ListRoom[0].Players.forEach(element => {
-      element.Viewer = false;
-    })
-    let list = []
-    for (let i = 3; i <= 15 ; i++) {
-      list.push({ value: i, type: 'co' }, { value: i, type: 'ro' }, { value: i, type: 'chuon' }, { value: i, type: 'bich' })
-    }
-    list = list.sort(() => Math.random() - 0.5);
     ListRoom.forEach(element => {
       if (element.idRoom === data.idRoom) {
+        element.Players.forEach((element, index) => {
+          element.Viewer = false;
+        })
+        let list = []
+        for (let i = 3; i <= 15; i++) {
+          list.push({ value: i, type: 'co' }, { value: i, type: 'ro' }, { value: i, type: 'chuon' }, { value: i, type: 'bich' })
+        }
+        list = list.sort(() => Math.random() - 0.5);
         element.Players.forEach((item, index) => {
           if (item.socketId === socket.id) item.ready = true
         })
         let start = true
-        element.Players.forEach((item, index) => {
+        element.Players.forEach((item) => {
           if (!item.ready) start = false;
         })
         if (start) {
-          element.playing = true;
+          ListRoom[0].playing = true
           element.Players.forEach((item, index) => {
             item.cards = list.slice(index * 13, (index + 1) * 13);
             item.totalCards = 13;
-            io.to(item.socketId).emit('test', { listCards: list.slice(index * 13, (index + 1) * 13), isTurn: ListRoom[0].isTurn})
+            io.to(item.socketId).emit('test', { listCards: list.slice(index * 13, (index + 1) * 13), isTurn: ListRoom[0].isTurn })
           })
         }
+        return;
       }
     })
-    if(ListRoom[0].playing)
-    io.sockets.emit('updateIsTurn', ListRoom[0].isTurn);
   })
-  socket.on('sendCard', (data) => { 
-    let array = [];
-    ListRoom[0].Players.forEach(element => {
-      if(!element.Viewer)
-      array.push(element)
+  socket.on('sendCard', (data) => {
+    ListRoom.forEach(value => {
+      if (value.idRoom === data.idRoom) {
+        let array = [];
+        value.Players.forEach(element => {
+          if (!element.Viewer)
+            array.push(element)
+        })
+        var i = data.index + 1;
+        const length = array.length;
+        while (true) {
+          if (i === length) i = 0;
+          if (!array[i].skip)
+            break;
+          i++
+        }
+        value.isTurn = i;
+        io.sockets.emit('updateIsTurn', i)
+        value.Players.forEach(element => {
+          if (element.userName === data.userName) element.totalCards -= data.listCard.length;
+        })
+        value.Players.forEach(element => {
+          io.to(element.socketId).emit('sendCard', data)
+        })
+      }
     })
-    var i = data.index + 1;
-    const length = array.length;
-    while (true) {
-      if (i === length) i = 0;
-      if(!array[i].skip) 
-        break;
-      i++
-    }
-    ListRoom[0].isTurn = i;
-    io.sockets.emit('updateIsTurn', i)
-    ListRoom[0].Players.forEach(element => {
-      if(element.userName  === data.userName) element.totalCards -= data.listCard.length;
-    })
-    socket.broadcast.emit('sendCardBroadcast', data);
   })
   socket.on('skip', data => {
-    let array = []
-    ListRoom[0].Players.forEach((element) => {
-      if (element.userName === data.userName)
-        element.skip = true;
+    ListRoom.forEach(value => {
+      if (value.idRoom === data.idRoom) {
+        let array = []
+        value.Players.forEach((element) => {
+          if (element.userName === data.userName)
+            element.skip = true;
+        })
+        value.Players.forEach(element => {
+          if (!element.Viewer)
+            array.push(element)
+        })
+        var i = data.index + 1;
+        const length = array.length;
+        while (true) {
+          if (i === length) i = 0;
+          if (!array[i].skip)
+            break;
+          i++
+        }
+        value.isTurn = i;
+        io.sockets.emit('updateIsTurn', i);
+        let count = 0;
+        array.forEach(element => {
+          if (element.skip)
+            count++;
+        })
+        value.Players.forEach(element => {
+          io.to(element.socketId).emit('skip', data.userName)
+        })
+        if (count + 1 === array.length) {
+          value.Players.forEach(element => {
+            io.to(element.socketId).emit('resetSkip')
+          })
+          value.Players.forEach(element => {
+            element.skip = false;
+          })
+        }
+        return;
+      }
     })
-    ListRoom[0].Players.forEach(element => {
-      if(!element.Viewer)
-      array.push(element)
-    })
-    var i = data.index + 1;
-    const length = array.length;
-    while (true) {
-      if (i === length) i = 0;
-      if(!array[i].skip) 
-        break;
-      i++
-    }
-    ListRoom[0].isTurn = i;
-    io.sockets.emit('updateIsTurn', i);
-    let count = 0;  
-    array.forEach(element => {
-      if (element.skip)
-        count++;
-    })
-    io.sockets.emit('skip', data.userName)
-    if (count + 1 === array.length) {
-      io.sockets.emit('resetSkip')
-      ListRoom[0].Players.forEach(element => {
-        element.skip = false;
-      })
-    }
-    console.log(ListRoom[0].isTurn)
   })
-  
   socket.on('gameOver', (data) => {
-    ListRoom[0].Players.forEach(element => {
-      if(element.userName === data) console.log(data)
+    ListRoom.forEach(element => {
+      if(element.idRoom === data.idRoom) {
+        element.Players.forEach(value => {
+          io.to(value.socketId).emit('gameOver', data.userName)
+        })
+      }
     })
-    io.sockets.emit('gameOver', data)
   })
-
   socket.on('logoutRoom', (data) => {
     ListRoom.forEach(element => {
       if (element.idRoom === data.idRoom) {
         element.Players = element.Players.filter(item => {
           return item.socketId !== socket.id;
         })
+        io.sockets.emit('updateRoom', getRoom(ListRoom))
       }
       io.sockets.emit('updatePlayer', element.Players)
+      
     })
   })
   socket.on("disconnect", function () {
-    ListRoom[0].Players = ListRoom[0].Players.filter(item => {
-      return item.socketId !== socket.id;
+    ListRoom.forEach(element => {
+      element.Players = element.Players.filter(item => {
+        return item.socketId !== socket.id;
+      })
     })
-    ListRoom[0].playing = false;
+    ListRoom = ListRoom.filter(item => {
+      return item.Players.length > 0
+    })
+    io.sockets.emit('updateRoom', getRoom(ListRoom))
     console.log('Ngắt kết nối vs ' + socket.id)
-    io.sockets.emit('updatePlayer', ListRoom[0].Players)
   })
 });
-
+function getRoom (ListRoom) {
+  const array = []
+  ListRoom.forEach(element => {
+    const password = element.password === '' ? false : true
+    array.push({nameRoom: element.nameRoom, idRoom: element.idRoom, coin: element.coin, songuoi: element.Players.length, maxnguoi: element.amount, password: password})
+  })
+  return array;
+}
 
 
 app.use(function (req, res, next) {
@@ -175,13 +209,13 @@ app.get('/', (req, res) => {
   res.send('hello app heroku!')
 })
 app.get('/getRooms', (req, res) => {
-  res.send(ListRoom)
+  res.send(getRoom(ListRoom))
 })
 
 app.post('/getPlayer', (req, res) => {
   ListRoom.forEach((element) => {
     if (element.idRoom === req.body.idRoom) {
-      res.send({Players: element.Players, play: element.playing})
+      res.send({ Players: element.Players, play: element.playing })
     }
   })
 })
@@ -189,7 +223,7 @@ app.post('/getPlayer', (req, res) => {
 
 app.post('/login', (req, res) => {
   const username = req.body.username;
-  const password = req.body.password; 
+  const password = req.body.password;
   const accessToken = jwt.sign(req.body, 'levanchuong', {
     expiresIn: '30m'
   })
